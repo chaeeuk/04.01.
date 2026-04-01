1306cfa6390e010d6cef806f03e52a2f5fab1321
chaeeuk <chaehusla@gmail.com>
2026-04-01 11:22:56 +0900

    five

---
+++ World.java
@@ -0,0 +1,10 @@
+public class World {
+    public static void run() {
+        System.out.println("202212116");
+    }
+
+    public static void run2() {
+        System.out.println("hello world");
+    }
+
+}
